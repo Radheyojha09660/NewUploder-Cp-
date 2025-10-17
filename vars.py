f14c92d520d6ffe3455ab97eb0f72640d3f7171b
Radheyojha09660 <ojha.radhey1995@gmail.com>
2025-10-17 13:44:08 +0530

    Update API credentials and owner details

--- vars.py
+++ vars.py
@@ -1,14 +1,14 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+#Radhey 
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "26704085"))
+API_HASH = environ.get("API_HASH", "f150646c78f09b4f88bef191a22539c0")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8438567514:AAHDHeyMy72S-Nra2HPDXLDLuNcWh8gOEDk")
 
-OWNER = int(environ.get("OWNER", ""))
-CREDIT = environ.get("CREDIT", "")
+OWNER = int(environ.get("OWNER", "7640198485"))
+CREDIT = environ.get("CREDIT", "7640198485")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
@@ -20,3 +20,4 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
